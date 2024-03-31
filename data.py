3c061f9191d2367b10b3ac9a378883c4f224cad1
javoliang <137265733+javoliang@users.noreply.github.com>
2024-03-31 11:59:19 -0500

    data.py

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-urban_routes_url = 'https://cnt-973c29fe-9fa9-47e2-bed7-731d57f7e808.containerhub.tripleten-services.com?lng=es'
+urban_routes_url = 'https://cnt-921c60ae-7011-492a-a15e-ab7ce8f1c320.containerhub.tripleten-services.com?lng=es'
 address_from = 'East 2nd Street, 601'
 address_to = '1300 1st St'
 phone_number = '+1 123 123 12 12'
