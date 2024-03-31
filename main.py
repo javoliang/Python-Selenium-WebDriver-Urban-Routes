199350c78d6f27bdb3730f2c7acb500d46f7b403
javoliang <137265733+javoliang@users.noreply.github.com>
2024-03-31 12:00:24 -0500

    main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import data
 import time
 import unittest
+import pytest_sugar
 from selenium import webdriver
 from selenium.webdriver import Keys
 from selenium.webdriver.common.action_chains import ActionChains
@@ -36,7 +37,6 @@ def retrieve_phone_code(driver) -> str:
                             "en tu aplicación.")
         return code
 
-
 class UrbanRoutesPage:
     from_field = (By.ID, 'from')
     to_field = (By.ID, 'to')
@@ -56,10 +56,7 @@ class UrbanRoutesPage:
 
 
 class TestUrbanRoutes(unittest.TestCase):
-
-    driver = webdriver.Chrome()
     phone_number = data.phone_number
-
     @classmethod
     def setup_class(cls):
         # no lo modifiques, ya que necesitamos un registro adicional habilitado
@@ -72,7 +69,7 @@ class TestUrbanRoutes(unittest.TestCase):
         chrome_options.add_argument('--perfLoggingPrefs=enableNetwork')
         cls.driver = webdriver.Chrome(options=chrome_options)
 
-    def test_set_route(self):
+    def test_step_1(self):
         self.driver.get(data.urban_routes_url)
         routes_page = UrbanRoutesPage(self.driver)
         address_from = data.address_from
@@ -83,12 +80,13 @@ class TestUrbanRoutes(unittest.TestCase):
             EC.visibility_of_element_located(routes_page.from_field)
         )
         from_element.send_keys(address_from)
-
         to_element = WebDriverWait(self.driver, 10).until(
             EC.visibility_of_element_located(routes_page.to_field)
         )
         to_element.send_keys(address_to)
-
+        assert routes_page.get_from() == address_from
+        assert routes_page.get_to() == address_to
+    def test_step_2(self):
         # Paso 2: Seleccionar la tarifa Comfort.
         comfort_button = WebDriverWait(self.driver, 10).until(
             EC.element_to_be_clickable((By.XPATH, '//button[@class="button round" and text()="Pedir un taxi"]'))
@@ -98,31 +96,27 @@ class TestUrbanRoutes(unittest.TestCase):
             EC.visibility_of_element_located((By.XPATH, '//img[@alt="Comfort"]'))
         )
         comfort_image.click()
-
+        assert comfort_image.is_displayed()
+    def test_step_3(self):
         # Paso 3: Rellenar el número de teléfono
         phone_number_element = WebDriverWait(self.driver, 10).until(
             EC.visibility_of_element_located((By.XPATH, "//div[text()='Número de teléfono']"))
         )
         phone_number_element.click()
-
         phone_label = WebDriverWait(self.driver, 10).until(
             EC.visibility_of_element_located((By.XPATH, "//label[text()='Número de teléfono']"))
         )
         phone_label.click()
-
         phone_input = WebDriverWait(self.driver, 10).until(
             EC.visibility_of_element_located((By.ID, "phone"))
         )
         phone_input.send_keys(self.phone_number)
-
         submit_button = WebDriverWait(self.driver, 10).until(
             EC.element_to_be_clickable((By.XPATH, "//button[@class='button full' and text()='Siguiente']"))
         )
         submit_button.click()
-
         # Obtener el código de confirmación del teléfono
         self.confirmation_code = retrieve_phone_code(self.driver)
-
         # Introducir el código de confirmación y confirmar
         code_label = WebDriverWait(self.driver, 10).until(
             EC.element_to_be_clickable((By.XPATH, "//label[@for='code' and text()='Introduce el código']"))
@@ -138,8 +132,8 @@ class TestUrbanRoutes(unittest.TestCase):
             EC.element_to_be_clickable((By.XPATH, "//button[@class='button full' and text()='Confirmar']"))
         )
         submit_button.click()
-        time.sleep(1)
-
+        assert submit_button.is_enabled()
+    def test_step_4(self):
         # Paso 4: Agregar una Tarjeta de Crédito
         payment_method_element = WebDriverWait(self.driver, 10).until(
             EC.visibility_of_element_located((By.XPATH, "//div[@class='pp-text' and text()='Método de pago']"))
@@ -152,14 +146,12 @@ class TestUrbanRoutes(unittest.TestCase):
         plus_button.click()
         card_number = data.card_number
         card_code = data.card_code
-
         # Introducir el número de tarjeta y el código
         card_number_element = WebDriverWait(self.driver, 10).until(
             EC.visibility_of_element_located((By.ID, "number"))
         )
         card_number_element.send_keys(card_number)
         time.sleep(1)
-
         # Simular presionar la tecla TAB para cambiar el enfoque y agregar el código de la tarjeta
         ActionChains(self.driver).send_keys(Keys.TAB).perform()
         time.sleep(1)
@@ -171,21 +163,19 @@ class TestUrbanRoutes(unittest.TestCase):
         time.sleep(1)
         ActionChains(self.driver).send_keys(Keys.TAB).perform()
         time.sleep(1)
-
         # Localizar el botón "Agregar" y hacer clic en él
         add_button = WebDriverWait(self.driver, 10).until(
             EC.element_to_be_clickable((By.XPATH, "//button[@class='button full' and text()='Agregar']"))
         )
         add_button.click()
         time.sleep(1)
-
         # Cerrar el cuadro de diálogo para continuar
         close_button = WebDriverWait(self.driver, 10).until(
             EC.element_to_be_clickable((By.XPATH, '//*[@id="root"]/div/div[2]/div[2]/div[1]/button'))
         )
         close_button.click()
-        time.sleep(1)
-
+        assert close_button.is_enabled()
+    def test_step_5(self):
         # Paso 5: Escribir un mensaje para el controlador
         message_for_driver = data.message_for_driver
         comment_element = WebDriverWait(self.driver, 10).until(
@@ -196,15 +186,20 @@ class TestUrbanRoutes(unittest.TestCase):
             EC.visibility_of_element_located((By.ID, "comment"))
         )
         comment_input.send_keys(message_for_driver)
-        time.sleep(1)
+        assert comment_input.get_property('value') == message_for_driver
 
+    def test_step_6(self):
         # Paso 6: Pedir una manta y pañuelos
         slider_element = WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, "//div[@class='r-sw-container']//span[@class='slider round']"))
+            EC.element_to_be_clickable((By.CSS_SELECTOR,
+                                        "#root > div > div.workflow > div.workflow-subcontainer > "
+                                        "div.tariff-picker.shown > div.form > div.reqs.open > "
+                                        "div.reqs-body > div:nth-child(1) > div > div.r-sw > div > "
+                                        "span.slider.round"))
         )
         slider_element.click()
-        time.sleep(1)
-
+        assert slider_element.is_enabled()
+    def test_step_7(self):
         # Paso 7: Pedir 2 helados
         counter_plus_element = WebDriverWait(self.driver, 10).until(
             EC.element_to_be_clickable((By.XPATH, "//div[@class='r-counter']//div[@class='counter-plus']"))
@@ -212,22 +207,23 @@ class TestUrbanRoutes(unittest.TestCase):
         counter_plus_element.click()
         time.sleep(1)
         counter_plus_element.click()
-        time.sleep(1)
-
-        # Modal para buscar un taxi
+        assert counter_plus_element.is_enabled()
+    def test_step_8(self):
+        # Paso 8 Modal para buscar un taxi
         taxi_button = WebDriverWait(self.driver, 10).until(
             EC.element_to_be_clickable((By.XPATH, "//span[@class='smart-button-main' and text()='Pedir un taxi']"))
         )
         taxi_button.click()
+        assert taxi_button.is_enabled()
         time.sleep(3)
-
-        # Mostrar Detalles
+    def test_step_9(self):
+        # Paso 9 Mostrar Detalles
         detail_button = WebDriverWait(self.driver, 10).until(
             EC.element_to_be_clickable((By.XPATH, "//img[@src='/static/media/burger.7f0605c2.svg' and @alt='burger']"))
         )
         detail_button.click()
+        assert detail_button.is_enabled()
         time.sleep(20)
-
     @classmethod
     def teardown_class(cls):
         # Cerrar la instancia del navegador al finalizar las pruebas
